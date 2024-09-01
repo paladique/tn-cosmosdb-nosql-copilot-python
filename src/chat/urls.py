7ad428a816495540413b69a107d1087cf13c37e0
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-09-02 00:47:30 +0300

    resolved url conflict

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('session/create/', views.create_session, name='create_session'),
+    path('session/<uuid:session_id>/', views.session_detail, name='session_detail'),
     path('session/<uuid:session_id>/add_message/', views.add_message, name='add_message'),
     path('session/<uuid:session_id>/update_message/<uuid:message_id>/', views.update_message, name='update_message'),
     path('cache/check/', views.check_and_cache, name='check_and_cache'),
