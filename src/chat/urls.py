dead5912ef2a9f0e8bb6dc53d5e45fb3eb15cd33
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-09-02 00:41:49 +0300

    uuid included

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('session/create/', views.create_session, name='create_session'),
-    path('session/<str:session_id>/add_message/', views.add_message, name='add_message'),
-    path('session/<str:session_id>/update_message/<str:message_id>/', views.update_message, name='update_message'),
-    path('cache/check_and_cache/', views.check_and_cache, name='check_and_cache'),
+    path('session/<uuid:session_id>/add_message/', views.add_message, name='add_message'),
+    path('session/<uuid:session_id>/update_message/<uuid:message_id>/', views.update_message, name='update_message'),
+    path('cache/check/', views.check_and_cache, name='check_and_cache'),
 ]
