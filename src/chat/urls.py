6f1c34cec3d31781e54e75ceac9bd7e48dd2e50b
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-09-01 23:38:20 +0300

    urls

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('session/create/', views.create_session, name='create_session'),
+    path('session/<str:session_id>/add_message/', views.add_message, name='add_message'),
+    path('session/<str:session_id>/update_message/<str:message_id>/', views.update_message, name='update_message'),
+    path('cache/check_and_cache/', views.check_and_cache, name='check_and_cache'),
 ]
